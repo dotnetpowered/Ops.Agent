4076f7369051ef7fc576eb9318b8083af00f12ad
Brian Ritchie <brianr@xeohealth.com>
2022-08-17 08:40:02 -0400

    Added vSphere agent

---
+++ Ops.Agents.vSphere/vSphereAgent.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using System.Management.Automation;
+using System.Reflection;
+using System.Text.Json;
+using System.Xml.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace Ops.Agents.vSphere;
+
+public class vSphereAgent : IOpsAgent
+{
+    private readonly ILogger<vSphereAgent> _logger;
+    private readonly IOpsIngestApi _ingestApi;
+
+    public vSphereAgent(ILogger<vSphereAgent> logger, IOpsIngestApi ingestApi)
+    {
+        _logger = logger;
+        _ingestApi = ingestApi;
+    }
+
+    public string SourceName => "vsphere";
+
+    public async Task CollectAsync(AgentConfig agentConfig)
+    {
+        PowerShell ps = PowerShell.Create();
+        //ps.Runspace.SessionStateProxy.SetVariable("a", new int[] { 1, 2, 3 });
+        //ps.AddScript("$a");
+        //ps.AddCommand("foreach-object");
+        //ps.AddParameter("process", ScriptBlock.Create("$_ * 2"));
+
+        // Load query from embedded resource
+        var assembly = Assembly.GetExecutingAssembly();
+        var stream = assembly.GetManifestResourceStream("vmware_serverlist.ps1");
+        if (stream == null)
+            throw new InvalidOperationException("Unable to load vmware_serverlist.ps1 resource.");
+        var streamreader = new StreamReader(stream);
+        var query = streamreader.ReadToEnd();
+        query = query.Replace("{{USER_NAME}}", agentConfig.Username);
+        query = query.Replace("{{PASSWORD}}", agentConfig.Password);
+        query = query.Replace("{{SERVER}}", agentConfig.Server);
+        ps.AddScript(query);
+
+        Collection<PSObject> results = ps.Invoke();
+        if (ps.HadErrors)
+        {
+            foreach (var err in ps.Streams.Error)
+            {
+                Console.WriteLine(err.Exception);
+            }
+        }
+        PSObject result = results.Skip(1).First();
+        var obj = JsonSerializer.Deserialize<JsonElement>(result.ToString());
+        //Name, NumCpu, MemoryGB, PowerState, Id, Notes, ProvisionedSpaceGB, UsedSpaceGB, CreateDate, DrsAutomationLevel
+        //@{ Name = 'OSName'; Expression ={$_.Guest.OSFullName}; }, `
+        //@{ Name = 'IpAddress'; Expression ={$_.Guest.IPAddress}; }, `
+        //@{ Name = 'HostName'; Expression ={$_.Guest.HostName}; }, `
+        //@{ Name = 'GuestState'; Expression ={$_.Guest.State}; }, `
+        //@{ Name = 'GuestFamily'; Expression ={$_.Guest.GuestFamily}; }, `
+        //@{ Name = 'VmVersion'; Expression ={$_.Version}; }, `
+        //@{ Name = 'VmHostId'; Expression ={$_.VMHost.Id}; }, `
+        //@{ Name = 'VmHostName'; Expression ={$_.VMHost.Name}; } `
+        var machines = from element in obj.EnumerateArray()
+                       where GetString(element, "HostName")!=null
+                       select new VirtualMachine(
+                                    GetString(element, "Id"),
+                                    GetString(element, "HostName"))
+                       {
+                           Description = GetString(element, "Name"),
+                           NumCpu = GetInt(element, "NumCpu"),
+                           MemoryGB = ((int?)GetDouble(element, "MemoryGB")),
+                           PowerState = GetIntAsString(element, "PowerState"),
+                           ProvisionedSpaceGB = GetDouble(element, "ProvisionedSpaceGB"),
+                           UsedSpaceGB = GetDouble(element, "UsedSpaceGB"),
+                           Notes = GetString(element, "Notes"),
+                           OSName = GetString(element, "OSName"),
+                           VmHostId = GetString(element, "VmHostId"),
+                           VmVersion = GetIntAsString(element, "VmVersion"),
+                           VmHostName = GetString(element, "VmHostName"),
+                           DrsAutomationLevel = GetIntAsString(element, "DrsAutomationLevel"),
+                           GuestFamily = GetString(element, "GuestFamily"),
+                           GuestState = GetIntAsString(element, "GuestState"),
+                           CreateDate = GetStringAsDateTime(element, "CreateDate")
+                       };
+        //await dbClient.UpsertItemsAsync("Metrics", "Items", machines);
+    }
+
+    string GetHostName(JsonElement element)
+    {
+        var hostname = GetString(element, "HostName");
+        if (string.IsNullOrEmpty(hostname))
+            return "missing";
+        else
+            return hostname;            
+    }
+
+    string? GetString(JsonElement element, string propertyName)
+    {
+        if (element.TryGetProperty(propertyName, out JsonElement prop))
+            return prop.GetString();
+        else
+            return null;
+    }
+
+    int? GetInt(JsonElement element, string propertyName)
+    {
+        if (element.TryGetProperty(propertyName, out JsonElement prop))
+            return prop.GetInt32();
+        else
+            return null;
+    }
+
+    double? GetDouble(JsonElement element, string propertyName)
+    {
+        if (element.TryGetProperty(propertyName, out JsonElement prop))
+            return prop.GetDouble();
+        else
+            return null;
+    }
+
+    string? GetIntAsString(JsonElement element, string propertyName)
+    {
+        if (element.TryGetProperty(propertyName, out JsonElement prop))
+            return prop.GetInt32().ToString();
+        else
+            return null;
+    }
+
+    DateTimeOffset? GetStringAsDateTime(JsonElement element, string propertyName)
+    {
+        if (element.TryGetProperty(propertyName, out JsonElement prop))
+            return DateTimeOffset.Parse(prop.GetString());
+        else
+            return null;
+    }
+
+}
+
---
+++ Ops.Agents.vSphere/vmware_serverlist.ps1
@@ -0,0 +1,13 @@
+﻿Connect-VIServer -Server {{SERVER}} -Protocol https `
+    -User {{USER_NAME}} -Password {{PASSWORD}} -Force
+Get-VM * | Select-Object `
+    Name, NumCpu, MemoryGB, PowerState, Id, Notes, ProvisionedSpaceGB, UsedSpaceGB, CreateDate, DrsAutomationLevel, `
+    @{Name='OSName';Expression={$_.Guest.OSFullName};}, `
+    @{Name='IpAddress';Expression={$_.Guest.IPAddress};}, `
+    @{Name='HostName';Expression={$_.Guest.HostName};}, `
+    @{Name='GuestState';Expression={$_.Guest.State};}, `
+    @{Name='GuestFamily';Expression={$_.Guest.GuestFamily};}, `
+    @{Name='VmVersion';Expression={$_.Version};}, `
+    @{Name='VmHostId';Expression={$_.VMHost.Id};}, `
+    @{Name='VmHostName';Expression={$_.VMHost.Name};} `
+    | ConvertTo-Json 
