99cab2b6236b48d23495ca51fcca504f16dc0d3b
Brian Ritchie <brianr@xeohealth.com>
2022-09-19 16:47:33 -0400

    Add Google Cloud Agent

---
+++ Ops.Agents.Google.Cloud/GoogleComputeAgent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Google.Cloud.Compute.V1;
+using Microsoft.Extensions.Logging;
+
+namespace Ops.Agents.Google.Cloud;
+
+public class GoogleComputeAgent : IOpsAgent
+{
+    private readonly ILogger<GoogleComputeAgent> _logger;
+    private readonly IOpsIngestApi _ingestApi;
+
+    public GoogleComputeAgent(ILogger<GoogleComputeAgent> logger, IOpsIngestApi ingestApi)
+    {
+        _logger = logger;
+        _ingestApi = ingestApi;
+    }
+
+    public string SourceName => "Google.Compute";
+
+    public Task CollectAsync(AgentConfig agentConfig)
+    {
+        throw new NotImplementedException();
+    }
+}
+
